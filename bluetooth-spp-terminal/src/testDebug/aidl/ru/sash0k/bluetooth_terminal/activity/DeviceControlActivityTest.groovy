d862177cf831cedbfb117ca779324f65a69cdd49
Marcel <linusdufe@gmail.com>
2018-05-28 10:50:54 +0200

    All buttons implemented and working correctly. Next steps:  - change buttons layout  - make it cooler and intuitive  - Implement extras  - Clean code  - Presentation

---
+++ bluetooth-spp-terminal/src/testDebug/aidl/ru/sash0k/bluetooth_terminal/activity/DeviceControlActivityTest.groovy
@@ -0,0 +1,4 @@
+package ru.sash0k.bluetooth_terminal.activity
+
+class DeviceControlActivityTest extends groovy.util.GroovyTestCase {
+}
---
+++ bluetooth-spp-terminal/src/testDebug/aidl/ru/sash0k/bluetooth_terminal/bluetooth/BluetoothUtilsTest.groovy
@@ -0,0 +1,4 @@
+package ru.sash0k.bluetooth_terminal.bluetooth
+
+class BluetoothUtilsTest extends groovy.util.GroovyTestCase {
+}
